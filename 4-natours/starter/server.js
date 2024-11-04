87f93c8a956065bc891674fdbd56c5ca3644960b
Vishal Kajale <vishal.kajale@hexafoldtech.com>
2024-11-04 12:20:18 +0530

    connected to Database.

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose'); // Add this line to require mongoose
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-
+// Connect to database
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+// const DB = process.env.DATABASE_LOCAL;
+mongoose.connect(DB, {}).then(() => {
+  console.log('Connected to database');
+});
 console.log(app.get('env'));
 // START SERVER
 const port = process.env.PORT || 3000;

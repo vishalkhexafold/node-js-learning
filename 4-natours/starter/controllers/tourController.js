b029d89687d685cf2b1568cccdc50b5de78fbfa9
Vishal Kajale <vishal.kajale@hexafoldtech.com>
2024-11-06 12:14:07 +0530

    Crated script to add,remove data in database.

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,31 +1,4 @@
-// const fs = require('fs');
-
 const Tour = require('../models/tourModels');
-
-// const tour = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
-// );
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       message: 'Tour not found',
-//       status: 'error'
-//     });
-//   }
-
-//   next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       message: 'Please provide all required fields',
-//       status: 'error'
-//     });
-//   }
-//   next();
-// };
 
 exports.getAllTours = async (req, res) => {
   try {

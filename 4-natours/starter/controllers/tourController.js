5f788ded04d362877e0f0cfd37fb4493cea41ad6
Vishal Kajale <vishal.kajale@hexafoldtech.com>
2024-11-09 11:58:32 +0530

    learn aggregation.

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -104,3 +104,105 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: {
+          ratingAverage: { $gte: 4.5 }
+        }
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          num: { $sum: 1 },
+          averageRating: { $avg: '$ratingAverage' },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+
+      {
+        $sort: { price: -1 }
+      },
+
+      {
+        $match: {
+          _id: { $ne: 'easy' }
+        }
+      }
+    ]);
+
+    res.status(201).json({
+      message: 'Stats fetched  successfully',
+      data: {
+        tour: stats
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: 'Stats not found',
+      status: error
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: {
+          month: '$_id'
+        }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: {
+          numTourStarts: -1
+        }
+      },
+      {
+        $limit: 12
+      }
+    ]);
+
+    res.status(201).json({
+      message: 'Stats fetched  successfully',
+      data: {
+        tour: plan
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: 'Stats not found',
+      status: error
+    });
+  }
+};
--- 4-natours/starter/models/tourModels.js
+++ 4-natours/starter/models/tourModels.js
@@ -1,86 +1,98 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose'); // Add this line to require mongoose
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: [true, 'A tour must have a name'],
-    trim: true
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a maximum group size']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty level']
-  },
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true
-  },
-  description: {
-    type: String,
-    required: [true, 'A tour must have a description'],
-    trim: true
-  },
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: [true, 'A tour must have a name'],
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a maximum group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty level']
+    },
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true
+    },
+    description: {
+      type: String,
+      required: [true, 'A tour must have a description'],
+      trim: true
+    },
 
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
 
-  priceDiscount: {
-    type: Number,
-    default: 0
-  },
+    priceDiscount: {
+      type: Number,
+      default: 0
+    },
 
-  rating: {
-    type: Number,
-    required: true,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    required: true,
-    default: 0,
-    select: false
-  },
-  ratingAverage: {
-    type: Number,
-    required: true,
-    default: 4.5
-  },
-  imageCover: {
-    type: String,
-    required: true,
-    default:
-      'https://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg'
-  },
-  images: [
-    {
+    rating: {
+      type: Number,
+      required: true,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      required: true,
+      default: 0,
+      select: false
+    },
+    ratingAverage: {
+      type: Number,
+      required: true,
+      default: 4.5
+    },
+    imageCover: {
       type: String,
       required: true,
       default:
         'https://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg'
-    } // Add this line to add an array of images
-  ],
-  startDates: [
-    {
-      type: Date
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+        default:
+          'https://st4.depositphotos.com/17828278/24401/v/450/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available.jpg'
+      } // Add this line to add an array of images
+    ],
+    startDates: [
+      {
+        type: Date
+      }
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now
     }
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      toObject: {
+        virtuals: true
+      }
+    }
   }
+);
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
-
 const Tour = mongoose.model('Tour', tourSchema);
 module.exports = Tour;
